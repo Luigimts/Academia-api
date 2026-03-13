f15124caadaa84d727a5befa9d3e688ba8c6dc71
Luigi Mattos <luigi.simao@aluno.senai.br>
2026-03-13 19:57:37 -0300

    progessor no login

--- academia-front/src/App.jsx
+++ academia-front/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Banner from "./componentes/Banner";
 import Footer from "./componentes/Footer";
@@ -9,18 +9,19 @@ import Instrutores from "./pages/Instrutor";
 import Planos from "./pages/Planos";
 import Agendamentos from "./pages/Agendamentos";
 
-// Novas páginas de login/dashboard
+// Login e dashboard
 import InstrutorLogin from "./pages/instrutorLogin";
 import InstrutorDashboard from "./pages/InstrutorDashboard";
 import AlunoLogin from "./pages/AlunoLogin";
-import AlunoDashboard from "./pages/AlunoDashboard"; // ainda vamos criar
+import AlunoDashboard from "./pages/AlunoDashboard";
+
 
 function App() {
   return (
-    <div>
-      <Banner /> {/* Banner separado */}
+    <>
 
-      {/* Navbar */}
+      <Banner />
+
       <nav style={{ display: "flex", justifyContent: "center", gap: "2rem", margin: "1rem 0" }}>
         <Link to="/alunos">Alunos</Link>
         <Link to="/instrutores">Instrutores</Link>
@@ -30,25 +31,26 @@ function App() {
         <Link to="/login-aluno">Login Aluno</Link>
       </nav>
 
-      {/* Rotas */}
       <Routes>
         <Route path="/alunos" element={<Alunos />} />
         <Route path="/instrutores" element={<Instrutores />} />
         <Route path="/planos" element={<Planos />} />
         <Route path="/agendamentos" element={<Agendamentos />} />
 
-        {/* Login e Dashboard do instrutor */}
+        {/* Instrutor */}
         <Route path="/instrutor/login" element={<InstrutorLogin />} />
         <Route path="/instrutor/:id" element={<InstrutorDashboard />} />
 
-        {/* Login e Dashboard do aluno */}
+        {/* Aluno */}
+        <Route path="/" element={<AlunoLogin />} />
         <Route path="/login-aluno" element={<AlunoLogin />} />
+        <Route path="/dashboard" element={<AlunoDashboard />} />
         <Route path="/aluno/:id" element={<AlunoDashboard />} />
       </Routes>
 
       <Footer />
-    </div>
+
+    </>
   );
 }
-
 export default App;
